b5e0d57acf46ec7f3b57ecdcdfef5ca47404c399
Igor Yaskin <yaskinigorek@icloud.com>
2021-09-21 00:20:15 +0300

    Login/Registration UI

--- LoginDemo/ViewControllers/Login/LoginViewController.swift
+++ LoginDemo/ViewControllers/Login/LoginViewController.swift
@@ -9,13 +9,44 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupUI()
+    }
+    
+    private func setupUI() {
+        self.title = "Вход"
+        loginButton.layer.cornerRadius = loginButton.bounds.height / 2
+        loginButton.layer.borderWidth = 1
+        loginButton.layer.borderColor = UIColor.black.cgColor
+        
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.setNavigationBarHidden(false, animated: animated)
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
 
+    @IBAction func loginButtonTapped(_ sender: UIButton) {
+        
+    }
+}
+
+extension LoginViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false
+    }
+    
 }
--- LoginDemo/ViewControllers/Registration/RegistrationViewController.swift
+++ LoginDemo/ViewControllers/Registration/RegistrationViewController.swift
@@ -9,13 +9,41 @@ import UIKit
 
 class RegistrationViewController: UIViewController {
 
+    @IBOutlet weak var registrationButton: UIButton!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupUI()
+    }
+    
+    private func setupUI() {
+        self.title = "Регистрация"
+        registrationButton.layer.cornerRadius = registrationButton.bounds.height / 2
+        registrationButton.layer.borderWidth = 1
+        registrationButton.layer.borderColor = UIColor.black.cgColor
+        
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.setNavigationBarHidden(false, animated: animated)
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
 
+}
+
+extension RegistrationViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false
+    }
+    
 }
